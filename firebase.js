b7320810b84904723b5fe819a05148ca432f912c
Lailah Grant <lailahgrant19@gmail.com>
2021-11-02 12:23:09 +0300

    FAILED SUFF

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
-{/* <script type="module"> */}
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
+// Import the functions you need from the SDKs you need
+    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
+    import {GoogleAuthProvider}  from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
+    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -18,6 +18,24 @@
   };
 
   // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-// </script>
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+
+    // add event listeners
+        document.getElementById('signin').addEventListener('click', GoogleLogin)
+
+        //For Authentication logic
+        //1) Create a provider -- Google Authentication object
+        let provider = new GoogleAuthProvider() 
+
+        //.then - js promises, pass them. it will return a callback ie res is passed in the console.log(res) or used in other places 
+        
+        export function GoogleLogin(){
+            console.log('Signin button clicked');
+            //2) caall the logic for auth
+            signInWithPopup(provider).then(res => { //auth is a predefined method with in firebase, pass a provider in the signInWith... 
+                console.log(res)
+            }).catch(e => { //catch incase there's an issue 
+                console.log(e)
+            })   
+        }
