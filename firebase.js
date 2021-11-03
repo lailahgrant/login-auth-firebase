c4da7b3b8298c0073bf8f75d8aa023f03ed6a0ab
Lailah Grant <lailahgrant19@gmail.com>
2021-11-03 07:28:06 +0300

    finally added firebase init to my project, put my html files in public folder

--- firebase.js
+++ firebase.js
@@ -1,12 +1,4 @@
-// Import the functions you need from the SDKs you need
-    import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
-    import {GoogleAuthProvider}  from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
-    import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
     apiKey: "AIzaSyCfAyb7INxl0u7Y9ha8VIT-gDqCybs_VI0",
     authDomain: "trial-db-ee9d4.firebaseapp.com",
